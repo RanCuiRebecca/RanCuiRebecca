b5c566d72514235c13d5d0948a67af5e3c2a15e4
Ran Cui <rancui@andrew.cmu.edu>
2019-10-29 14:58:15 -0400

    assign 6b

--- assgn6/productDetail.js
+++ assgn6/productDetail.js
@@ -1,10 +1,14 @@
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
 if(document.readyState == 'loading'){
   document.addEventListener('DOMContentLoaded', ready);
 } else{
   ready();
 }
+
+
 
 function ready(){
   getFromLocalStorage()
@@ -29,6 +33,37 @@ function ready(){
 
 }
 
+
+// Next/previous controls
+function plusSlides() {
+  showSlides(++slideIndex );
+}
+function minusSlides() {
+  showSlides(--slideIndex);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length ) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+
+}
 
 function checkOutButtonClicked(event){
   var totalPriceElement = document.getElementsByClassName("totalMoney")[0];
