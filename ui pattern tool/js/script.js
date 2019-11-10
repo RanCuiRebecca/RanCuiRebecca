58a3d691da437e46cb64f9507f66c063530d7f62
Ran <43759128+RanCuiRebecca@users.noreply.github.com>
2019-11-09 23:19:20 -0500

    capstone
    capstone

---
+++ ui pattern tool/js/script.js
@@ -0,0 +1,7 @@
+const options = {
+    linkSelector:
+      'a[href^="' +
+      window.location.origin +
+      '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])'
+  };
+var swup = new Swup(options);
